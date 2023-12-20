46190071d29e2f7283331c0f6c88fb3bcd5fee80
Yurii Kazakov <gfnvortex@gmail.com>
2023-12-20 14:29:44 +0200

    hw_1_init

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 'use strict';
 
-let userName = prompt('Hello! What is your name?');
+const userName = prompt('Hello! What is your name?');
 
-alert('Hello! My name is: ' + userName);
+alert('Hello! My name is: ' + userName + '!');
